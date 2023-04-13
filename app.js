6f9f7202d9a3f0b6173f52b7767b5c262bdf28ec
Jean-Marie N'Dah <jndah@3701-C02DH4ZSP3YV.local>
2023-04-13 11:46:13 -0400

    Included a database in the node js code

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname+"/date.js");
 const app = express();
+const _ = require("lodash");
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/todoList');
+
+const itemSchema =new mongoose.Schema({
+    name:String
+}); 
+
+const Item = mongoose.model('Item', itemSchema);
+
+const listSchema =new mongoose.Schema({
+    listName:String,
+    items:[itemSchema]
+}); 
+
+const list = mongoose.model('List', listSchema);
 
 app.set('view engine', 'ejs');
 
@@ -9,35 +24,104 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.use(express.static("public"));
 
-let item = "";
+var defaultList;
+var ExistingList;
 
-const items = [];
+async function insertDefaultItemList() {
+    const exist = await Item.find({}).lean().exec();
+    if (exist.length === 0) {
+        const item1 = new Item({name:"First item"});
+        const item2 = new Item({name:"Second item"});
+        const item3 = new Item({name:"Third item"});
 
-const workItems = [];
+    await Item.insertMany([
+        item1,
+        item2,
+        item3
+    ]);
+    }
+
+    defaultList= await Item.find({}).lean().exec();
+}
+
+async function listExist(listName) {
+    ExistingList =   await list.findOne({ listName: listName }).exec();
+}
+
+async function createList(listitem) {
+    await listitem.save();
+}
+
+async function removeItem(itemId) {
+  await Item.deleteOne({ _id: itemId });
+}
+
+async function updateListItem(listName, itemId) {
+    await list.updateOne({ listName: listName }, {$pull:{items : {_id : itemId}}},{ isDeleted: true });
+}
+
 
 app.get("/", function(request, response){
-
-    let day = date.getDate();
-
-    response.render("list", {listTitle: day, listOfItem : items });
-
+    insertDefaultItemList().then(function () {
+    response.render("list", {listTitle: "Today", listOfItem : defaultList });
+    });
 }) 
 
 app.post("/", function(request, response) {
 
-    item = request.body.listItem ;
-    if (request.body.button === "Work") {
-        workItems.push(item);
-        response.redirect("work");
+    if (request.body.button === "Today") {
+    itemInserted = new Item({name:request.body.listItem});
+    itemInserted.save();
+    response.redirect("/");
     } else {
-        items.push(item);
-        response.redirect("/");
+        itemInserted = new Item({name:request.body.listItem});
+        listExist(request.body.button).then(function () {
+            if (ExistingList == null) {
+                response.redirect("/"+request.body.button);
+            } else {
+                ExistingList.items.push(itemInserted);
+                ExistingList.save();                
+                response.redirect("/"+request.body.button);
+            }
+        });
+    }
+
+});
+
+app.post("/delete", function(request, response) {
+
+    console.log(request.body.listName);
+    if (request.body.listName === "Today") {
+        removeItem(request.body.checkbox).then(function() {
+            response.redirect("/");
+        })
+    } else {
+        updateListItem(request.body.listName, request.body.checkbox).then(function() {
+            response.redirect("/"+request.body.listName);
+        });
     }
 });
 
-app.get("/work", function(request, response) {
-    response.render("list", {listTitle:"Work List", listOfItem:workItems});
-})
+app.get("/:listtName", function(request, response) {
+    const customListName = _.capitalize(request.params.listtName);
+    listExist(customListName).then(function() {
+        if ( ExistingList ==null ) {
+            insertDefaultItemList().then(function () {
+                const listItem = new list({
+                    listName:customListName,
+                    items:defaultList
+                });
+                createList(listItem).then(function() {
+                    response.render("list", {listTitle: customListName, listOfItem : defaultList });   
+                })
+            })
+        } else {
+            response.render("list", {listTitle: ExistingList.listName, listOfItem : ExistingList.items });   
+        }
+    });
+
+  });
+
 
 app.get("/about", function(request, response) {
     response.render("about");
